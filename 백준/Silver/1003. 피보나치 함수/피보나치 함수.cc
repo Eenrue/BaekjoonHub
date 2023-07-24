a52ab19feac154f5de0a07075a17f0cadb3193c3
Eenrue <128016413+Eenrue@users.noreply.github.com>
2023-07-24 17:18:37 +0900

    [Silver III] Title: 피보나치 함수, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.cc
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.cc
@@ -1,38 +1,24 @@
-#include<iostream>
-#include <vector>
-#include <algorithm>
+#include <iostream>
 using namespace std;
-int mem0[41] = {-1, };
-int mem1[41] = { -1, };
-
-int check0(int n) {
-	if (mem0[n] > 0) return mem0[n];
-	else if (n == 1)return 0;
-	else if (n == 0)return 1;
-	else {
-		return mem0[n]=check0(n-1)+check0(n-2);
-	}
-}
-int check1(int n) {
-	if (mem1[n] > 0) return mem1[n];
-	else if (n == 1)return 1;
-	else if (n == 0)return 0;
-	else {
-
-		return mem1[n] = check1(n - 1) + check1(n - 2);
-	}
-}
-
-int main()
+long long fiboarr[50] = {0,1,};
+long long fibo(int N)
 {
-	int N;
-	int  inp;
-	int cnt = 1;
-	cin >> N;
-	for (int i = 1; i <= N; i++) {
-		cin >> inp;
-		cout << check0(inp) << ' '<<check1(inp)<<'\n';
-	}
-
-	return 0;
+    if(N == 0 || N == 1)
+        return fiboarr[N];
+    else if(fiboarr[N] == 0)
+        fiboarr[N] = fibo(N-1) + fibo(N-2);
+    return fiboarr[N];
+}
+int main() {
+    int T;
+    cin >> T;
+    int tmp;
+    for(int i = 0; i < T; i++)
+    {
+        cin >> tmp;
+        if(tmp == 0)
+            cout << "1 0" << '\n';
+        else
+            cout << fibo(tmp-1) << ' ' << fibo(tmp) << '\n';
+    }
 }
