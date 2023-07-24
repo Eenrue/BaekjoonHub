ac0e46c0346c3f4fba5f2203be24794250c011f4
Eenrue <128016413+Eenrue@users.noreply.github.com>
2023-07-24 15:57:59 +0900

    [Silver II] Title: 스택 수열, Time: 52 ms, Memory: 2416 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.cc
+++ 백준/Silver/1874. 스택 수열/스택 수열.cc
@@ -1,48 +1,36 @@
 #include<iostream>
 #include <vector>
+#include <stack>
 #include <algorithm>
 using namespace std;
-int stack[10000] = { 0 };
-int k = -1;
 
 int main()
 {
 	int N;
-	int inp;
-	int ind;
-	int tmp=-1;
-	int eflag = 0;
-	cin >> N;
-	vector<int> vec;
+	stack<int> s;
 	vector<char> res;
-	for (int i = 1; i <= N; i++) {
-		vec.push_back(i);
-	}
+	int  inp;
+	int cnt = 1;
+	cin >> N;
 	for (int i = 1; i <= N; i++) {
 		cin >> inp;
-		if (eflag == 0) {
-			ind = (find(vec.begin(), vec.end(), inp) - vec.begin());
-			if (ind >= tmp) {
-				while (ind != tmp) {
-					res.push_back('+');
-					tmp++;
-				}
-				res.push_back('-');
-				vec.erase(find(vec.begin(), vec.end(), inp));
-				tmp--;
-			}
-			else {
-				eflag = 1;
-			}
+		while (inp >= cnt) {
+			s.push(cnt);
+			res.push_back('+');
+			cnt++;
+		}
+		if (inp == s.top()) {
+			s.pop();
+			res.push_back('-');
+		}
+		else {
+			cout << "NO";
+			return 0;
 		}
 	}
-	if (eflag == 1) cout << "NO";
-	else {
-		for (int i = 0; i < res.size(); i++) {
-			cout << res[i] << "\n";
-		}
+	for (int i = 0; i < res.size(); i++) {
+		cout << res[i] << "\n";
 	}
-
 
 
 	return 0;
