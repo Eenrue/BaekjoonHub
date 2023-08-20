b0634c536ed36214cb38e82aa3237e8b384dd395
Eenrue <128016413+Eenrue@users.noreply.github.com>
2023-08-20 14:59:15 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.cc
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.cc
@@ -1,31 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
 int tmp;
-bool cmp(int a, int b){
-    if(a>b)return true;
-    else return false;
-}
 
 int main()
 {
     ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
     int n;
     cin>>n;
-    vector<pair<int,int>> v;
+    int arr[1001];
+    int dp[1001];
+
     int ans=0;
     for(int i=0;i<n;i++){
+        dp[i]=1;
+        int tmp;
         cin>>tmp;
-        vector<int> tv;
-        for(int j=0;j<v.size();j++){
-            if(v[j].first<tmp)tv.push_back(v[j].second);
+        arr[i]=tmp;
+        for(int j=0;j<i;j++){
+            if(arr[j]<arr[i]){
+                dp[i]=max(dp[i],dp[j]+1);
+            }
         }
-        if(tv.size()){
-            sort(tv.begin(),tv.end(),cmp);
-            v.push_back({tmp,tv[0]+1});
-        }
-        else v.push_back({tmp,1});
-        if(v.back().second>ans) ans=v.back().second;
+        ans=max(ans,dp[i]);
     }
+
     cout<<ans;
     return 0;
 }
