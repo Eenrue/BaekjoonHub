e66d6144b2fee636234f46e28106e3494a4854e6
Eenrue <128016413+Eenrue@users.noreply.github.com>
2023-09-03 20:28:53 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 12 ms, Memory: 3020 KB -BaekjoonHub

---
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.cc
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define INF 987654321
+
+int n,k;
+bool vis[100001]={};
+int d[100001]={};
+
+void bfs(){
+    priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
+    pq.push({0,n});
+    d[n]=0;
+    while(!pq.empty()){
+        int dis=pq.top().first;
+        int cur=pq.top().second;
+        pq.pop();
+        if(d[cur]<dis)continue;
+        if(cur-1>=0){
+            if(dis+1<d[cur-1]){
+                d[cur-1]=dis+1;
+                pq.push({dis+1,cur-1});
+            }
+        }
+        if(cur+1<=100000){
+            if(dis+1<d[cur+1]){
+                d[cur+1]=dis+1;
+                pq.push({dis+1,cur+1});
+            }
+        }
+        if(2*cur<=100000){
+            if(dis<d[2*cur]){
+                d[2*cur]=dis;
+                pq.push({dis,2*cur});
+            }
+        }
+    }
+}
+
+
+
+int main()
+{
+
+    ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+    cin>>n>>k;
+    for(int i=0;i<100001;i++) d[i]=INF;
+
+    bfs();
+    cout<<d[k];
+
+
+    return 0;
+}
