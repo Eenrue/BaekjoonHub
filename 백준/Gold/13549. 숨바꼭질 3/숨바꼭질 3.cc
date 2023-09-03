04aa74fd68674e10d4c08180e7bf6d0b3e3e8140
Eenrue <128016413+Eenrue@users.noreply.github.com>
2023-09-03 20:44:44 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 0 ms, Memory: 2252 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.cc
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.cc
@@ -5,38 +5,38 @@ using namespace std;
 
 int n,k;
 bool vis[100001]={};
-int d[100001]={};
 
 void bfs(){
-    priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
-    pq.push({0,n});
-    d[n]=0;
-    while(!pq.empty()){
-        int dis=pq.top().first;
-        int cur=pq.top().second;
-        pq.pop();
+    deque<pair<int,int>> dq;
+    dq.push_back({n,0});
+
+    while(!dq.empty()){
+        int cur=dq.front().first;
+        int dis=dq.front().second;
+        dq.pop_front();
         if(cur==k){
-            cout<<d[cur];
+            cout<<dis;
             break;
         }
+        if(cur*2<100001){
+            if(!vis[cur*2]){
+                vis[cur*2]=true;
+                dq.push_front({cur*2,dis});
+            }
+        }
         if(cur-1>=0){
-            if(dis+1<d[cur-1]){
-                d[cur-1]=dis+1;
-                pq.push({dis+1,cur-1});
+            if(!vis[cur-1]){
+                vis[cur-1]=true;
+                dq.push_back({cur-1,dis+1});
             }
         }
-        if(cur+1<=100000){
-            if(dis+1<d[cur+1]){
-                d[cur+1]=dis+1;
-                pq.push({dis+1,cur+1});
+        if(cur+1<100001){
+            if(!vis[cur+1]){
+                vis[cur+1]=true;
+                dq.push_back({cur+1,dis+1});
             }
         }
-        if(2*cur<=100000){
-            if(dis<d[2*cur]){
-                d[2*cur]=dis;
-                pq.push({dis,2*cur});
-            }
-        }
+
     }
 }
 
@@ -47,7 +47,6 @@ int main()
 
     ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
     cin>>n>>k;
-    for(int i=0;i<100001;i++) d[i]=INF;
 
     bfs();
 
