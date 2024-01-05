c3ce76647624b939b3487df0eabbbcb0d156b53c
Eenrue <128016413+Eenrue@users.noreply.github.com>
2024-01-05 22:44:47 +0900

    [level 3] Title: 네트워크, Time: 0.02 ms, Memory: 3.77 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.cpp
+++ 프로그래머스/3/43162. 네트워크/네트워크.cpp
@@ -2,24 +2,22 @@
 #include <vector>
 
 using namespace std;
-vector<vector<int>> comp;
 bool visit[201]={};
 
-void dfs(int n, int num){
+void dfs(int n, int num,vector<vector<int>> &comp){
     visit[num]=true;
     for(int i=0;i<n;i++){
         if(comp[num][i]==1&&visit[i]==false){
-            dfs(n,i);
+            dfs(n,i,comp);
         }
     }
 }
 
 int solution(int n, vector<vector<int>> computers) {
     int answer = 0;
-    comp=computers;
     for(int i=0;i<n;i++){
         if(visit[i]==false){
-            dfs(n,i);
+            dfs(n,i,computers);
             answer++;
         }
     }
