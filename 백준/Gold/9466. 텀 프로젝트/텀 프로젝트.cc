9686f5174ff80dd005c63cfc8ba9f4a2524f61a7
Eenrue <128016413+Eenrue@users.noreply.github.com>
2024-02-05 17:27:49 +0900

    [Gold III] Title: 텀 프로젝트, Time: 548 ms, Memory: 5608 KB -BaekjoonHub

---
+++ 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.cc
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+# define INF 987654321
+typedef long long ll;
+
+int mp[100001];
+bool vis[100001];
+bool done[100001];
+int cyc=0;
+void check(int k){
+    if(done[k])return;
+    if(!vis[k]){
+        vis[k]=true;
+        check(mp[k]);
+
+    }
+    else{
+        cyc++;
+        done[k]=true;
+        check(mp[k]);
+    }
+    //cout<<'s';
+    done[k]=true;
+}
+
+int main()
+{
+    ios_base :: sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+    int t;
+    cin>>t;
+    for(int i=0;i<t;i++){
+        int n;
+        cin>>n;
+        vector<int> v;
+        cyc=0;
+        for(int j=1;j<=n;j++){
+            int tmp;
+            cin>>tmp;
+            mp[j]=tmp;
+        }
+        for(int j=1;j<=n;j++){
+            check(j);
+        }
+        cout<<n-cyc<<'\n';
+        memset(vis,0,sizeof(vis));
+        memset(done,0,sizeof(done));
+    }
+}
