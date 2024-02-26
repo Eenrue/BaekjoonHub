9d04020b2893d855d7ed68d2bf7ca466252d84da
Eenrue <128016413+Eenrue@users.noreply.github.com>
2024-02-26 22:30:34 +0900

    [level 3] Title: 여행경로, Time: 0.01 ms, Memory: 4.11 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/README.md
+++ 프로그래머스/3/43164. 여행경로/README.md
@@ -1,10 +1,10 @@
 # [level 3] 여행경로 - 43164 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43164?itm_content=course14743) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43164) 
 
 ### 성능 요약
 
-메모리: 4.2 MB, 시간: 0.01 ms
+메모리: 4.11 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 02월 26일 22:27:49
+2024년 02월 26일 22:30:32
 
 ### 문제 설명
 
--- 프로그래머스/3/43164. 여행경로/여행경로.cpp
+++ 프로그래머스/3/43164. 여행경로/여행경로.cpp
@@ -1,17 +1,37 @@
-#include <string>
-#include <vector>
+#include <bits/stdc++.h>
 using namespace std;
-map<string,vector<string>> mp;
+vector<vector<string>> v;
+bool vis[10001]={};
 int flag=false;
+int n;
+vector<string> ans;
+
+void dfs(vector<string> sv){
+    if(sv.size()==n+1){
+        flag=true;
+        ans=sv;
+        return;
+    }
+    string last=sv.back();
+    for(int i=0;i<n;i++){
+        if(flag)return;
+        if(v[i][0]==last&&!vis[i]){
+            string next=v[i][1];
+            vis[i]=true;
+            sv.push_back(next);
+            dfs(sv);
+            sv.pop_back();
+            vis[i]=false;
+        }
+    }
+    
+}
 
 vector<string> solution(vector<vector<string>> t) {
     vector<string> answer;
+    n=t.size();
     sort(t.begin(),t.end());
-    for(int i=0;i<t.size();i++){
-        mp[t[i][0]].push_back(t[i][1]);
-    }
-    for(auto iter=mp.begin();iter!=mp.end();iter++){
-        string str=iter->first;
-    }
-    return answer;
+    v=t;
+    dfs({"ICN"});
+    return ans;
 }
